55cbeaa9bf72c98546671aa31bd972ac1a66b366
Herivin Simmasalam <hsimma0@gmail.com>
2022-05-24 19:02:21 -0400

    First Commit

---
+++ server.js
@@ -0,0 +1,21 @@
+// DEPENDECIES
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const mongoose = require('mongoose');
+const PORT = 3021;
+// MOUNT MIDDLEWARE
+//ROUTES
+// I
+// N
+// D
+// U
+// C
+// E
+// S
+
+
+// LISTENER
+app.listen(PORT, () => {
+    console.log(`Hi I am Andre, and I am listening to : ${PORT}`)
+})
